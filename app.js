140b66ccfc9481c1bb31d7f4881ec9506e0f1c87
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2021-03-09 14:30:43 +0200

    Added titles output to console

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-var banner = document.getElementById('page-banner');
-var bookList = document.getElementById('book-list');
+var titles = document.getElementsByClassName('title')
 
-console.log(banner, bookList); 
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(item){
+    console.log(item);
+})
